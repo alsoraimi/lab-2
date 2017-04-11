fa5ad7673702c9c7addfab9aa0cd0edb0a05ea41
alsoraimi <alsoraimi46@gmail.com>
2017-04-11 14:17:52 -0400

    add code to print ODD #

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -10,6 +10,31 @@ namespace Lab2
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Please enter a integer between 1 and 100");
+
+            int Num;
+            Num = int.Parse(Console.ReadLine());          // user input
+
+            if (Num % 2 != 0)
+
+
+            {
+
+                Console.WriteLine(Num);
+                Console.WriteLine("ODD");
+
+
+
+            }
+
+
+
+
+
+
+
+
         }
     }
 }
