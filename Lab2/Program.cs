7b5e5599d483113f566d54b704a5048dbc8849b8
alsoraimi <alsoraimi46@gmail.com>
2017-04-11 15:13:06 -0400

    added loop to make continuous if user wants

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -10,41 +10,56 @@ namespace Lab2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter your first name");
-            string Name;
-            Name = Console.ReadLine();
-
-
-            Console.WriteLine("Hello {0}, Please enter a integer between 1 and 100", Name);
-
-            int Num;
-            Num = int.Parse(Console.ReadLine());          // user input
-
-            if (Num % 2 != 0 && Num > 60) 
-
-
+            bool askToContinue = false;
+            do
             {
 
-                Console.WriteLine(Num);
-                Console.WriteLine("Your number is ODD, {0}", Name);
-            }
+                Console.WriteLine("Please enter your first name");
+                string Name;
+                Name = Console.ReadLine();
 
-          if (Num <= 25 && Num >= 2 && Num % 2 == 0) 
 
-            {
+                Console.WriteLine("Hello {0}, Please enter a integer between 1 and 100", Name);
 
-                Console.WriteLine("Your number is EVEN and less than 25, {0}", Name);
-            }
+                int Num;
+                Num = int.Parse(Console.ReadLine());          // user input
 
-            if (Num % 2 == 0 && Num > 60)
-            {
-                Console.WriteLine(Num);
-                Console.WriteLine("your number is EVEN, {0}" , Name);
+                if (Num % 2 != 0 && Num > 60)
 
-            }
+
+                {
+
+                    Console.WriteLine(Num);
+                    Console.WriteLine("Your number is ODD, {0}", Name);
+                }
+
+                if (Num <= 25 && Num >= 2 && Num % 2 == 0)
+
+                {
+
+                    Console.WriteLine("Your number is EVEN and less than 25, {0}", Name);
+                }
+
+                if (Num % 2 == 0 && Num > 60)
+                {
+                    Console.WriteLine(Num);
+                    Console.WriteLine("your number is EVEN, {0}", Name);
+
+                }
+                Console.WriteLine("do you want to try again, {0} ?" + " " + "(Y/N)", Name);
+                if (Console.ReadLine().ToUpper() == "Y")
+                    askToContinue = true;
+                else
+                {
+                    return;
+                }
+
+
+
+
+            } while (askToContinue);
+
             
-            
-
 
 
 
